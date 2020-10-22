5e0af199e7daab7ead481f1ce00ee04008b94430
Ryan Kelley <kelleyr@duvalschools.org>
2020-10-22 16:12:01 -0400

    Update operators0.0.py

--- operators/operators0.0.py
+++ operators/operators0.0.py
@@ -1,1 +1,13 @@
 # This code sample will help students develop an understanding of the most common operators used in Python.
+
+# my_int0 
+# my_int1 
+
+# my_float0  
+# my_float1 
+
+# my_string0 
+# my_string1
+
+# my_bool0
+# my_bool1
