0d9dd11bda3759e6ec05b5bcd947fd632ac3f744
Ryan Kelley <kelleyr@duvalschools.org>
2020-10-22 16:44:44 -0400

    v0.35
    Added operators for strings.  Removed some newline characters.

--- operators/operators_basic_data_types.py
+++ operators/operators_basic_data_types.py
@@ -1,4 +1,4 @@
-# This code sample will help students develop an understanding of the most common operators used in Python. v0.3
+# This code sample will help students develop an understanding of the most common operators used in Python. v0.35
 import time
 
 my_int0 = 125340234
@@ -7,13 +7,13 @@ my_int1 = -152
 my_float0 = 5.02451 
 my_float1 = -1254.44322252999
 
-my_string0 = "This is a strong string.\n"
-my_string1 = "This is a weak string.\n"
+my_string0 = "This is a strong string."
+my_string1 = "This is a weak string."
 
 my_bool0 = True
 my_bool1 = False
 
-print("Arithmetic Operators with Integers\n")
+print("\n\nArithmetic Operators with Integers\n")
 print("Addition: " , my_int0 + my_int1)
 print("Subtraction: ", my_int0 - my_int1)
 print("Multiplication: ", my_int0 * my_int1)
@@ -21,10 +21,19 @@ print("Division: ", my_int0 / my_int1)
 print("Modulus: ", my_int0 % my_int1)
 time.sleep(5)
 
-print("Arithmetic Operators with Floats\n")
+print("\n\nArithmetic Operators with Floats\n")
 print("Addition: " ,my_float0 + my_float1)
 print("Subtraction: ", my_float0 - my_float1)
 print("Multiplication: ", my_float0 * my_float1)
 print("Division: ", my_float0 / my_float1)
 print("Modulus: ", my_float0 % my_float1)
 time.sleep(5)
+
+print("\n\nArithmetic Operators with Strings\n")
+print("Addition: ", my_string0 + my_string1)
+# print("Subtraction: ", my_string0 - my_string1)
+print("Multiplication: ", my_string0 * 5)
+# print("Division: ", my_string0 / my_string1)
+# print("Modulus: ", my_string0 % my_string1)
+time.sleep(5)
+
