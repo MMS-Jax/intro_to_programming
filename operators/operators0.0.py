11ab6e21f5e288b8aeb3cdf28cdf1cdcdb44e325
Ryan Kelley <kelleyr@duvalschools.org>
2020-10-22 16:21:45 -0400

    v0.3
    Added basic arithmetic operators for Integers and Floats.

--- operators/operators0.0.py
+++ operators/operators0.0.py
@@ -1,13 +1,14 @@
 # This code sample will help students develop an understanding of the most common operators used in Python.
 
-# my_int0 
-# my_int1 
+my_int0 = 125340234
+my_int1 = -152
 
-# my_float0  
-# my_float1 
+my_float0 = 5.02451 
+my_float1 = -1254.44322252999
 
-# my_string0 
-# my_string1
+my_string0 = "This is a strong string.\n")
+my_string1 = "This is a weak string.\n")
 
-# my_bool0
-# my_bool1
+my_bool0 = True
+my_bool1 = False
+
---
+++ operators/operators_basic_data_types.py
@@ -0,0 +1,30 @@
+# This code sample will help students develop an understanding of the most common operators used in Python. v0.3
+import time
+
+my_int0 = 125340234
+my_int1 = -152
+
+my_float0 = 5.02451 
+my_float1 = -1254.44322252999
+
+my_string0 = "This is a strong string.\n"
+my_string1 = "This is a weak string.\n"
+
+my_bool0 = True
+my_bool1 = False
+
+print("Arithmetic Operators with Integers\n")
+print("Addition: " , my_int0 + my_int1)
+print("Subtraction: ", my_int0 - my_int1)
+print("Multiplication: ", my_int0 * my_int1)
+print("Division: ", my_int0 / my_int1)
+print("Modulus: ", my_int0 % my_int1)
+time.sleep(5)
+
+print("Arithmetic Operators with Floats\n")
+print("Addition: " ,my_float0 + my_float1)
+print("Subtraction: ", my_float0 - my_float1)
+print("Multiplication: ", my_float0 * my_float1)
+print("Division: ", my_float0 / my_float1)
+print("Modulus: ", my_float0 % my_float1)
+time.sleep(5)
