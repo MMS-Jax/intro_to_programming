19e1bbf6f71a4b34df8cd296562a19900052a950
Ryan Kelley <kelleyr@duvalschools.org>
2020-11-04 23:28:16 -0500

    Update cpu_tester.py

--- iterations/while_loops/cpu_tester.py
+++ iterations/while_loops/cpu_tester.py
@@ -1,19 +1,23 @@
 # CPU Tester, Ryan Kelley, v0.1
 import time
 
-start_time = time.time()
-x = 0
-while x < 1000:
-    x += 1
-    print(f"{x:,}\n")
-end_time = time.time()
-print(f"It took {end_time - start_time} seconds to count up to {x}.")
-time.sleep(5)
+for i in range(3):
+    start_time0 = time.time()
+    x = 0
+    while x < 100:
+        x += 1
+        print(f"{x:,}\n")
+    end_time0 = time.time()
+    print(f"It took {end_time0 - start_time0} seconds to count up to 1,000.\n")
+    time.sleep(5)
 
-start_time = time.time()
-x = 0
-while x < 1000:
-    x = x + 1
-    print(f"{x:,}\n")
-end_time = time.time()
-print(f"It took {end_time - start_time} seconds to count up to {x}.")
+    start_time1 = time.time()
+    while x != 0:
+        x += -1
+        print(f"{x:,}\n")
+    end_time1 = time.time()
+    print(f"It took {end_time1 - start_time1} seconds to count down from 1,000.\n")
+    print(f"This is loop {i + 1}.")
+    time.sleep(3)
+    
+    
